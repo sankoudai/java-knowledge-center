61c3239b30543aff8ba24912dd8dada782dff89d
sankoudai <798245503@qq.com>
2015-08-05 15:57:49 +0800

    commit TestString

---
+++ java-knowledge-basics/src/main/java/basics/type/TestString.java
@@ -0,0 +1,51 @@
+package basics.type;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+
+import java.util.Arrays;
+
+/**
+ * @author : sankoudai
+ * @version : created at 2015/8/5
+ */
+public class TestString extends TestCase {
+    public void testType(){
+        String str = "abc";
+        Assert.assertTrue(str instanceof String);
+
+        Object stringObj = "abc";
+        Assert.assertTrue(stringObj instanceof String);
+    }
+
+    /**whitespace + line endings will be removed*/
+    public void testTrim() {
+        String line = "  abc\n";
+        System.out.println(line + " END ");
+        System.out.println(line.trim() + " END ");
+    }
+
+    public void testSubstring(){
+        System.out.println("*" + "0123".substring(4) + "*");
+    }
+
+    /**split(regex) */
+    public void testSplit(){
+        /* Single spaced words */
+        String line = "word0 word1 word2 SingleSpace NoTrailing";
+        String[] strs = line.split(" ");
+        System.out.println(Arrays.toString(strs));
+
+        /* Double spaced word */
+        System.out.println("*******Double spaced word******");
+        line = "word0  word1  word2  DoubleSpace  NoTrailing";
+        strs = line.split(" +");
+        System.out.println(Arrays.toString(strs));
+
+        /* Multiple spaced word */
+        System.out.println("*******Multiple spaced word******");
+        line = "word0  word1  word2  DoubleSpace  NoTrailing";
+        strs = line.split("[ ]+");
+        System.out.println(Arrays.toString(strs));
+    }
+}
