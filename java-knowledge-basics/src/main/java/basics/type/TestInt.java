a1434a689d1afd5f7fdab54c3eeba9f1e7369fa9
sankoudai <798245503@qq.com>
2015-08-06 18:17:01 +0800

    TestInt minor change

--- java-knowledge-basics/src/main/java/basics/type/TestInt.java
+++ java-knowledge-basics/src/main/java/basics/type/TestInt.java
@@ -7,6 +7,14 @@ import junit.framework.TestCase;
  * @version : created at 2015/8/5
  */
 public class TestInt extends TestCase {
+    public void testValue(){
+        int i = 10;
+        System.out.printf("int : %d \n", i);
+
+        int hexInt = 0xff;
+        System.out.printf("int: 0xff=%d \n", hexInt);
+    }
+
     public void testMaxValue(){
         int maxInt = Integer.MAX_VALUE;
         System.out.printf("maxInt = %d, about 2billion \n", maxInt);
