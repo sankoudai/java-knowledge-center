f1e4027267577e526fd6dc0e87417e53aff07784
jinagtao <zhaojiangtao_zjt@sina.com>
2016-04-05 15:35:27 +0800

    时间解析格式

--- java-knowledge-api/src/main/java/com/sankoudai/java/api/time/TestDate.java
+++ java-knowledge-api/src/main/java/com/sankoudai/java/api/time/TestDate.java
@@ -36,6 +36,20 @@ public class TestDate extends TestCase {
         System.out.printf("testSetTime (after setTime): date = %s \n", date);
     }
 
+    /**
+     * Date and Time Pattern 	        Result
+     * "yyyy.MM.dd G 'at' HH:mm:ss z" 	2001.07.04 AD at 12:08:56 PDT
+     * "EEE, MMM d, ''yy" 	            Wed, Jul 4, '01
+     * "h:mm a" 	                    12:08 PM
+     * "hh 'o''clock' a, zzzz" 	        12 o'clock PM, Pacific Daylight Time
+     * "K:mm a, z" 	                    0:08 PM, PDT
+     * "yyyyy.MMMMM.dd GGG hh:mm aaa" 	02001.July.04 AD 12:08 PM
+     * "EEE, d MMM yyyy HH:mm:ss Z" 	Wed, 4 Jul 2001 12:08:56 -0700
+     * "yyMMddHHmmssZ" 	                010704120856-0700
+     * "yyyy-MM-dd'T'HH:mm:ss.SSSZ" 	2001-07-04T12:08:56.235-0700
+     *
+     * @throws ParseException
+     */
     public void testConversionWithString() throws ParseException {
         /*from string*/
         String str = "2014-7-24 12:00:00";
