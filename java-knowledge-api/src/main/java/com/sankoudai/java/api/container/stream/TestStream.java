f9d8e56ee1bc68a8d13dd8923328a06ab8281b0d
sankoudai <798245503@qq.com>
2017-11-15 15:51:42 +0800

    stream example: copy by collect

--- java-knowledge-api/src/main/java/com/sankoudai/java/api/container/stream/TestStream.java
+++ java-knowledge-api/src/main/java/com/sankoudai/java/api/container/stream/TestStream.java
@@ -16,6 +16,13 @@ public class TestStream extends TestCase {
         System.out.println("sum is:" + nums.stream().filter(num -> num != null).
                         distinct().mapToInt(num -> num * 2).
                         peek(System.out::println).skip(2).limit(4).sum());
+
+        //使用stream进行copy
+        nums = Arrays.asList(1, 1, null, 2, 3, 4, null, 5, 6, 7, 8, 9, 10);
+        List<Integer> copy = nums.stream().collect(Collectors.toList());
+        copy.add(11);
+        System.out.println(nums);
+        System.out.println(copy);
     }
 
     public void test(){
